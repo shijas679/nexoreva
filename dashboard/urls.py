7708319f68923f4ff677c147c6ab5bf4d8386c14
abhijithllai <llaiabhijith@gmail.com>
2025-08-05 15:15:13 +0530

    views staff

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import dashboard_view
+from .views import dashboard_view, user_login
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
+    path('login/', user_login, name='login'),
 ]
