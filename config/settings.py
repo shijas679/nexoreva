2d1a22025388a5ba0ee1ddb47d785ad8b4604607
muhammed <muhammedshifan475@gmail.com>
2025-08-07 11:31:27 +0530

    add status

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     # Third-party
     'crispy_forms',
     'workassignment',
+    'task_trakking'
     
 
 
---
+++ task_trakking/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('task-status', views.task_status, name='task-status')
+]
